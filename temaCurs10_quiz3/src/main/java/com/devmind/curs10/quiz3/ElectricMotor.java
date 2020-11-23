4e23a9398cb050792d4a300a5cd6595b8cf3f624
VlasceanuMihai <mihai.vlasceanu1996@gmail.com>
2020-11-23 23:26:57 +0200

    Am adaugat temaCurs10_quiz3 in propriul branch

---
+++ temaCurs10_quiz3/src/main/java/com/devmind/curs10/quiz3/ElectricMotor.java
@@ -0,0 +1,14 @@
+package com.devmind.curs10.quiz3;
+
+/**
+ * Project: Curs10
+ * Author: mihai
+ * Date: 11/18/2020
+ */
+public class ElectricMotor extends Motor{
+
+    @Override
+    public PolutionCategory getPolutionCategory() {
+        return PolutionCategory.LOW;
+    }
+}
---
+++ temaCurs10_quiz3/src/test/java/com/devmind/curs10/quiz3/CarrFactoryIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.devmind.curs10.quiz3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+/**
+ * Project: Homework
+ * Author: mihai
+ * Date: 11/19/2020
+ */
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ConfigQuiz3.class, loader = AnnotationConfigContextLoader.class)
+class CarrFactoryIntegrationTest {
+
+    @Autowired
+    CarFactory carFactory;
+
+    @Test
+    void createCar_electric() {
+        Car electricCar = this.carFactory.createCar("Opel", "electric");
+        Assertions.assertEquals(PolutionCategory.LOW.toString(), electricCar.getPolutionCategory().toString());
+    }
+
+    @Test
+    void createCar_diesel(){
+        Car dieselCar = this.carFactory.createCar("Audi", "diesel");
+        Assertions.assertEquals(PolutionCategory.HIGH.toString(), dieselCar.getPolutionCategory().toString());
+    }
+
+    @Test
+    void createCar_petrol(){
+        Car petrolCar = this.carFactory.createCar("Bmw", "petrol");
+        Assertions.assertEquals(PolutionCategory.MEDIUM.toString(), petrolCar.getPolutionCategory().toString());
+    }
+
+    @Test
+    void createCar_nonExisted(){
+        try{
+            Car car = this.carFactory.createCar("Volvo", "carbuni");
+            Assertions.fail();
+        }catch (IllegalArgumentException e){
+            Assertions.assertEquals(IllegalArgumentException.class, e.getClass());
+        }
+    }
+}
